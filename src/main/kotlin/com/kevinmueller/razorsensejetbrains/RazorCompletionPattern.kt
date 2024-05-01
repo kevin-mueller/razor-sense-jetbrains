39c91b94bad5078d27d70079d57d9e3b52bc6472
kevin-mueller <kevin.mueller1@outlook.com>
2024-05-01 12:16:58 +0200

    at least it runs

--- src/main/kotlin/com/kevinmueller/razorsensejetbrains/RazorCompletionPattern.kt
+++ src/main/kotlin/com/kevinmueller/razorsensejetbrains/RazorCompletionPattern.kt
@@ -1,11 +1,10 @@
 package com.kevinmueller.razorsensejetbrains
 
-import com.intellij.lang.Language
 import com.intellij.lang.LanguageParserDefinitions
+import com.intellij.lang.LanguageUtil
 import com.intellij.patterns.PatternCondition
 import com.intellij.psi.PsiElement
 import com.intellij.psi.tree.TokenSet
-import com.intellij.psi.util.PsiUtilCore
 import com.intellij.util.ProcessingContext
 
 
@@ -17,13 +16,15 @@ import com.intellij.util.ProcessingContext
  */
 internal class RazorCompletionPattern : PatternCondition<PsiElement>("razorClassPattern()") {
     override fun accepts(psi: PsiElement, context: ProcessingContext): Boolean {
-        val language: Language = PsiUtilCore.findLanguageFromElement(psi)
-        val definition = LanguageParserDefinitions.INSTANCE.forLanguage(language) ?: return false
+        
+        val language = LanguageUtil.findRegisteredLanguage("Razor") ?: return false
 
+        val definition = LanguageParserDefinitions.INSTANCE.forLanguage(language) ?: return false
+        
+        
         // suggest completions in string and comment literals
         val tokens = TokenSet.orSet(
             definition.stringLiteralElements,
-            definition.commentTokens
         )
 
         val node = psi.node ?: return false
