716f41741dca1174807717653b7d71f30be1717a
Kevin <kevin.mueller@digitecgalaxus.ch>
2024-07-01 18:15:51 +0200

    fix completion pattern

--- src/main/kotlin/com/kevinmueller/razorsensejetbrains/RazorCompletionContributor.kt
+++ src/main/kotlin/com/kevinmueller/razorsensejetbrains/RazorCompletionContributor.kt
@@ -2,14 +2,19 @@ package com.kevinmueller.razorsensejetbrains
 
 import com.intellij.codeInsight.completion.*
 import com.intellij.patterns.PlatformPatterns
+import com.intellij.patterns.PlatformPatterns.psiElement
+import com.intellij.patterns.PsiElementPattern
 import com.intellij.patterns.XmlPatterns
+import com.intellij.psi.PsiElement
+import com.intellij.psi.xml.XmlAttribute
+import com.intellij.psi.xml.XmlElementType.*
 
 
 class RazorCompletionContributor : CompletionContributor() {
     init {
         extend(
             CompletionType.BASIC,
-            PlatformPatterns.psiElement().with(RazorCompletionPattern()), //TODO: this overrides the existing completion
+            psiElement().inside(XmlPatterns.xmlAttributeValue("Class")),
             RazorCompletionProvider()
         )
     }
