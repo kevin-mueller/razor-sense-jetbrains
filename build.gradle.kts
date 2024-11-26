c9eb2a1ef953269fc03cbda7fbaabdb906ca691d
Kevin <kevin.mueller@digitecgalaxus.ch>
2024-11-26 22:43:55 +0100

    try with empty untilbuild

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "com.kevinmueller"
-version = "1.0.9"
+version = "1.1.0"
 
 repositories {
     mavenCentral()
@@ -35,7 +35,7 @@ tasks {
 
     patchPluginXml {
         sinceBuild.set("232")
-        untilBuild.set("243.*")
+        untilBuild.set("")
     }
 
     signPlugin {
